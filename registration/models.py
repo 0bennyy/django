09dff9b1cfe962defebae1ebfc94413a073250c9
0bennyy <bmwanzo579@gmail.com>
2024-04-17 15:45:26 +0300

    new

---
+++ registration/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class client(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100)
